cf0ff4a56c82bca905af769c0bbcf3e1e60a42c9
Kartik Shukla <63971235+kartiks-21@users.noreply.github.com>
2020-09-14 18:22:37 +0530

    Update Automated Mailer.py

--- Automated Mailer.py
+++ Automated Mailer.py
@@ -1,17 +1,24 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
 host_addr = 'sender's address'
 receiver_addr = ['addr1','addr2','addr3']
+
 message = MIMEMultipart()
 message['from'] = host_addr
 message['To'] = ",".join(receiver_addr)
 message['subject'] = 'Testing mailer'
+
 body = 'This is a sample message for mailer testing'
+
 message.attach(MIMEText(body,'plain'))
+
 email="sender's address"
 password="sender's password"
+
 mail=smtplib.SMTP('smtp.gmail.com',587)
+
 mail.ehlo()
 mail.starttls()
 mail.login(email,password)
